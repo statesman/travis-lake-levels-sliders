3256c03f84f6e27ff418fa1422fcd47178bf758a
Andrew Chavez <adchavez@gmail.com>
2015-05-28 14:06:15 -0500

    Use imagesLoaded to fire then-and-now sliders

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,19 @@
 
   'use strict';
 
-  $(window).load(function() {
+  $(function() {
     $('.then-and-now').each(function(i, el) {
       var $slider = $(el);
-      $slider.twentytwenty();
-      var images = $el.children('img');
-      $slider.find('.twentytwenty-before-label').attr('data-content', $(images[0]).attr('data-date'));
-      $slider.find('.twentytwenty-after-label').attr('data-content', $(images[1]).attr('data-date'));
+      $slider.imagesLoaded()
+        .done(function() {
+          $slider.twentytwenty();
+          var images = $el.children('img');
+          $slider.find('.twentytwenty-before-label').attr('data-content', $(images[0]).attr('data-date'));
+          $slider.find('.twentytwenty-after-label').attr('data-content', $(images[1]).attr('data-date'));
+        })
+        .fail(function() {
+          $slider.remove();
+        });
     });
   });
 
